ae56fe1c0cb84492441f0367b305f3c923eea517
Tim Xie <pdt.xie@gmail.com>
2022-03-03 00:44:12 +1300

    added sugar final

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
-
-const API_URL = "https://thebusybeancafeapi.azurewebsites.net/";
+// const API_URL = "https://thebusybeancafeapi.azurewebsites.net/";
+const API_URL = "http://127.0.0.1:5000/";
 
 
 
