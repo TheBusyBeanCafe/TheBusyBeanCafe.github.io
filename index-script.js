139ae6cfd3105f399b17a9f77b4697ab7d85ed24
Tim Xie <pdt.xie@gmail.com>
2022-03-04 00:02:25 +1300

    use azure db

--- index-script.js
+++ index-script.js
@@ -1,5 +1,5 @@
-// const API_SHIFT_URL = "https://thebusybeancafeapi.azurewebsites.net/shifts"
-const API_SHIFT_URL = "http://127.0.0.1:5000/shifts";
+const API_SHIFT_URL = "https://thebusybeancafeapi.azurewebsites.net/shifts"
+// const API_SHIFT_URL = "http://127.0.0.1:5000/shifts";
 
 function getCurrentDate() {
 
