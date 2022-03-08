67ac44e9b98f896edf485e91def167976030257b
Reagan Bohan <xbjfk.github@gmail.com>
2022-03-08 23:06:37 +1300

    copokei again

--- orderpage-script.js
+++ orderpage-script.js
@@ -361,7 +361,7 @@ function coffeeClicked(idx) {
 		document.getElementById("add-or-edit-title").innerHTML = "New Order"
 		document.getElementById("delete").style.display = "none";
     } else {
-		var ckie = {index: idx, date: Date.now(), id: uuidv4(), count: 1, large: false}
+		var ckie = {index: idx, date: Date.now(), id: uuidv4(), count: 1, large: false, sugar: 0, payment: 0}
 		orderedCoffees.push(ckie);
 		fetch(API_URL + "transactions", {
 			method: "POST",
